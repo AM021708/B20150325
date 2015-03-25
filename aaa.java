6b41407246868672a73ef7e917631361a5cbcd57
AM021708 <y20051201@gmail.com>
2015-03-25 09:39:07 +0800

    Commit send

---
+++ aaa.java
@@ -0,0 +1,14 @@
+class aaa{
+	public static void main(String[] args){
+	int a=1;
+	int b=3;
+	int c=-10;
+	float aa,ab;
+	
+	double d = Math.pow(((b*b)-(4*a*c)),0.5);
+	aa=(float)(-(b)+d)/(2*a);
+	ab=(float)(-(b)-d)/(2*a);
+	//system out println("µª®×¨â­Ó®Ú:");
+	System.out.println(aa+", "+ab);
+	}
+}
